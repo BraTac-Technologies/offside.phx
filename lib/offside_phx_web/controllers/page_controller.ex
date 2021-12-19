29d6c8a4d940d4aea164974c04ce081d526e4106
Yasen Kostov <ykostov@pm.me>
2021-12-20 00:45:05 +0200

    alias post schema to page :admin controller

--- lib/offside_phx_web/controllers/page_controller.ex
+++ lib/offside_phx_web/controllers/page_controller.ex
@@ -1,11 +1,16 @@
 defmodule OffsidePhxWeb.PageController do
   use OffsidePhxWeb, :controller
 
+  alias OffsidePhx.Posts
+  #tags
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def admin(conn, _params) do
-    render(conn, "admin.html")
+    posts = Posts.list_posts()
+    # tags
+    render(conn, "admin.html", posts: posts)
   end
 end
